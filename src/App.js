e1b8f1df3bf46cd5c949dd42bf7c829602b4b8bf
MisenEko <48181258+MisenEko@users.noreply.github.com>
2021-10-22 16:46:41 +0200

    build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 function App() {
   return (
     <>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <NavBar />
         <Switch>
           <Route path ="/" exact component={Home} />
